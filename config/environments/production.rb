44456377e9dfbbd0090e22951188a20458a8551a
Frankz Kastner <frvnkzzz@gmail.com>
2014-06-02 02:02:56 -0500

    Correciones AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,14 +85,15 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'http://arcane-shore-2097.herokuapp.com/'}
 
   #Sets Paperclip to upload images to Amazon S3
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+Paperclip::Attachment.default_options.merge!({
+    storage: :s3,
+    s3_host_name: 's3-eu-west-1.amazonaws.com',
+    s3_credentials: {
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    bucket: ENV['S3_BUCKET_NAME']
+})
 
 
 end
